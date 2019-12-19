1503a13ca0d7361bba693477eee447fabc6e6884
TulupovArtem <tulupov1995@gmail.com>
2019-12-19 23:16:05 +0300

    fix .h file and add Makefile

--- include/asm.h
+++ include/asm.h
@@ -1,41 +1,13 @@
-.PHONY: all libft clean fclean re
+#ifndef ASM_H
+# define ASM_H
 
-OBJDIR = obj
-SRCDIR = src
-LIBDIR = libft
-HEADER = -I ./include
-HEADER_LIBFT = -I ./libft/include -I ./libft/src/ft_printf/include
-FLAGS = -Wall -Wextra -Werror
+# include "libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
 
-ASM = asm
-SRC_ASM_NAME = main.c
-
-OBJ_ASM_NAME = $(SRC_ASM_NAME:.c=.o);
-SRC_ASM = $(addprefix $(OBJDIR)/, $(SRC_ASM_NAME))
-OBJ_ASM = $(addprefix $(OBJDIR)/, $(OBJ_ASM_NAME))
-
-all: libft $(ASM)
-
-$(ASM): $(OBJ_ASM)
-	@gcc $(FLAGS) $^ -o $@ $(HEADER_LIBFT) $(HEADER) -L libft -lft
-
-$(OBJDIR)/%.o: $(SRCDIR)/%.c include/asm.h
-	@mkdir -p $(OBJDIR)
-	@gcc $(FLAGS) $(HEADER_LIBFT) $(HEADER) -o $@ -c $<
-
-libft:
-	@make -C $(LIBDIR)
-
-clean:
-	@make clean -C $(LIBDIR)
-	@rm -Rf $(OBJDIR)
-
-fclean: clean
-	@make fclean -C $(LIBDIR)
-	@rm -Rf $(ASM)
-
-re: fclean all
-
-norm:
-	norminette $(SRCDIR)
-	norminette ./include
+#endif
