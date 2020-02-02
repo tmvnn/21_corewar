87086c4bd1edbabf9328c780dca464a0002f5b14
Yoshiko Perra frey <yperra-f@il-d5.21-school.ru>
2020-02-02 18:58:20 +0300

    половина паттернов регулярных выражений написано

--- main.c
+++ main.c
@@ -7,16 +7,17 @@
 #define PATTERN_NAME_COMMENT "^ *.comment *\".{0,2048}\" *[#;].*$"
 #define PATTERN_COMMENT "^ *#.*$"
 #define PATTERN_ALTER_COMMENT "^ *;.*$"
-#define PATTERN_EMPTY_LINE "|"
-#define djfksljf "*r[0-9]{1,2}$ *[#;].*$"
+#define PATTERN_EMPTY_LINE ""
 #define PATTERN_LABEL "^ *[a-z0-9_]+: *((live|zjmp|fork|lfork) *(%-?0*[0-9]+|%:[0-9a-z_]+)|(ld|lld) *(%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *r[0-9]{1,2}|st *r[0-9]{1,2} *, *(r[0-9]{1,2}|-?[0-9]+|:[0-9a-z_]+)|(add|sub) *r[0-9]{1,2} *, *r[0-9]{1,2} *, *r[0-9]{1,2}|(and|or|xor) *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *r[0-9]{1,2}|(ldi|lldi) *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *(r[0-9]{1,2}|%-?0*[0-9]+$|^%:[0-9a-z_]+) *, *r[0-9]{1,2}|sti *r[0-9]{1,2} *, *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+)) *([#;].*| *)$"
+#define PATTERN_LABEL_SIMPLE "^ *[a-z0-9_]+: *([#;].*| *)$"
+
 
 int main(int ac, char **av){
 	int err;
 	regex_t reg1;
 	if (ac != 2)
 		return (0);
-	err = regcomp(&reg1, PATTERN_LABEL, REG_EXTENDED);
+	err = regcomp(&reg1, PATTERN_LABEL_SIMPLE, REG_EXTENDED);
 	if (err != 0){
 		printf("error with regex in file interpretation.c\n");
 		return (0);
