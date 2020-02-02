491c4d5e9ed1182529f2e6c42ae7a51f259b4264
Yoshiko Perra frey <yperra-f@il-d5.21-school.ru>
2020-02-02 17:40:45 +0300

    доработка регулярного выражения для парсинга строки

--- main.c
+++ main.c
@@ -9,25 +9,25 @@
 #define PATTERN_ALTER_COMMENT "^ *;.*$"
 #define PATTERN_EMPTY_LINE "|"
 #define djfksljf "*r[0-9]{1,2}$ *[#;].*$"
-#define PATTERN_LABEL "^ *[a-z0-9_]+: *(live *(%-?0*[0-9]+|%:[0-9a-z_]+)|ld *(%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+), *r[0-9]{1,2}|st *r[0-9]{1,2}, *(r[0-9]{1,2}|-?[0-9]+|:[0-9a-z_]+)|add *r[0-9]{1,2}, *r[0-9]{1,2}, *r[0-9]{1,2}|sub *r[0-9]{1,2}, *r[0-9]{1,2}, *r[0-9]{1,2}|and *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+), *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+), *r[0-9]{1,2}| ) *([#;].*| *)$"
+#define PATTERN_LABEL "^ *[a-z0-9_]+: *((live|zjmp|fork|lfork) *(%-?0*[0-9]+|%:[0-9a-z_]+)|(ld|lld) *(%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *r[0-9]{1,2}|st *r[0-9]{1,2} *, *(r[0-9]{1,2}|-?[0-9]+|:[0-9a-z_]+)|(add|sub) *r[0-9]{1,2} *, *r[0-9]{1,2} *, *r[0-9]{1,2}|(and|or|xor) *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *r[0-9]{1,2}|(ldi|lldi) *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *(r[0-9]{1,2}|%-?0*[0-9]+$|^%:[0-9a-z_]+) *, *r[0-9]{1,2}|sti *r[0-9]{1,2} *, *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+|-?[0-9]+|:[0-9a-z_]+) *, *(r[0-9]{1,2}|%-?0*[0-9]+|%:[0-9a-z_]+)) *([#;].*| *)$"
 
 int main(int ac, char **av){
-    int err;
-    regex_t reg1;
-    if (ac != 2)
-        return (0);
-    err = regcomp(&reg1, PATTERN_LABEL, REG_EXTENDED);
-    if (err != 0){
-        printf("error with regex in file interpretation.c\n");
-        return (0);
-    }
-    printf("%lu\n", strlen(*(av + 1)));
-    regmatch_t pm;
-    if (!regexec(&reg1, *(av + 1), 0, &pm, 0)){
-        printf("good match\n");
-        return (1);
-    } else{
-        printf("bad match\n");
-    }
-    return (0);
+	int err;
+	regex_t reg1;
+	if (ac != 2)
+		return (0);
+	err = regcomp(&reg1, PATTERN_LABEL, REG_EXTENDED);
+	if (err != 0){
+		printf("error with regex in file interpretation.c\n");
+		return (0);
+	}
+	printf("%lu\n", strlen(*(av + 1)));
+	regmatch_t pm;
+	if (!regexec(&reg1, *(av + 1), 0, &pm, 0)){
+		printf("good match\n");
+		return (1);
+	} else{
+		printf("bad match\n");
+	}
+	return (0);
 }
