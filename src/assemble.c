e6206e4f9dcd24f4b6db48bfa67cb4d111c30b53
Savinov Evgeny <Zsavinoww@gmail.com>
2020-02-18 17:41:33 +0300

    подредактировал, готовимся приводить к норме

--- src/assemble.c
+++ src/assemble.c
@@ -19,25 +19,19 @@ void	assemble(t_asm_content **content)
 	rows = NULL;
 	while (get_next_line((*content)->fd_src, &(*content)->line) > 0)
 	{
-		if (!((*content)->line = check_valid((*content), (*content)->fd_src))){
-			printf("not valid file\n");
+		if (!((*content)->line = check_valid((*content), (*content)->fd_src)))
 			error(*content);
-		}
 		tokenizing(&(*content)->line, &rows, content);
 	}
 	// what_are_strings(rows);
 	if (!(*content)->flag_pattern || !check_all_label(rows, content))
-	{
-		printf("not valid file\n");
 		error(*content);
-	}
 	(*content)->exec_code_size = (*content)->memory_code_size;
 	(*content)->asm_size = (*content)->exec_code_size + (*content)->header_size;
 	(*content)->bytecode = (char *)ft_memalloc((*content)->asm_size * sizeof(char));
 	ft_bzero((*content)->bytecode, (*content)->asm_size);
 	in_bytecode(content);
-	if (!fill_file(rows, content))
-		return ;
+	fill_file(rows, content);
 	write((*content)->fd_dst, (*content)->bytecode, (*content)->asm_size);
 	clean_memory(content);
 }
--- src/fill_file.c
+++ src/fill_file.c
@@ -7,16 +7,15 @@ int		check(t_token *pointer)
 	return (1);
 }
 
-int		fill_write(t_token *pointer, t_strings *rows, t_asm_content **content)
+void	fill_write(t_token *pointer, t_strings *rows, t_asm_content **content)
 {
 	fill_write_code_instraction(pointer->content, content);
 	if (check(pointer))
 		fill_write_code_arg(pointer, content);
 	fill_write_arg(pointer, rows, content);
-	return (1);
 }
 
-int		fill_file(t_strings *rows, t_asm_content **content)
+void		fill_file(t_strings *rows, t_asm_content **content)
 {
 	t_token		*pointer;
 	t_strings	*rows_start;
@@ -28,13 +27,9 @@ int		fill_file(t_strings *rows, t_asm_content **content)
 		while (pointer)
 		{
 			if (!ft_strcmp(pointer->type, INSTRACTION_NAME))
-			{
-				if (!fill_write(pointer, rows_start, content))
-					return (0);
-			}
+				fill_write(pointer, rows_start, content);
 			pointer = pointer->next;
 		}
 		rows = rows->next;
 	}
-	return (1);
 }
