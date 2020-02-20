d3a88239020b27c53b1c56fa12a452279bd31420
Iron emmett Dunaver <idunaver@il-f6.21-school.ru>
2020-02-20 20:04:59 +0300

    sldkfjhgsdf

--- src/rebase_str.c
+++ src/rebase_str.c
@@ -6,7 +6,7 @@
 /*   By: idunaver <idunaver@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 21:13:59 by idunaver          #+#    #+#             */
-/*   Updated: 2020/02/18 19:27:54 by idunaver         ###   ########.fr       */
+/*   Updated: 2020/02/20 19:55:58 by idunaver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,94 +15,94 @@
 char	*rebase_str_first_case(char *content)
 {
 	char *result;
-    char *str_cat;
+	char *str_cat;
 	char *temp;
-    char *temp1;
-	
+	char *temp1;
+
 	result = NULL;
-    str_cat = NULL;
+	str_cat = NULL;
 	temp = NULL;
-    temp1 = NULL;
+	temp1 = NULL;
 	temp = ft_strchr(content, 37);
-    temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
-    str_cat = ft_strjoinwcm(temp1, ",");
-    result = ft_strjoinwcm(str_cat, temp);
-    ft_strdel(&content);
+	temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
+	str_cat = ft_strjoinwcm(temp1, ",");
+	result = ft_strjoinwcm(str_cat, temp);
+	ft_strdel(&content);
 	return (result);
 }
 
 char	*rebase_str_second_case(char *content)
 {
 	char *result;
-    char *str_cat;
+	char *str_cat;
 	char *temp;
-    char *temp1;
-	
+	char *temp1;
+
 	result = NULL;
-    str_cat = NULL;
+	str_cat = NULL;
 	temp = NULL;
-    temp1 = NULL;
+	temp1 = NULL;
 	temp = ft_strchr(content, 58) + 1;
-    temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
-    str_cat = ft_strjoinwcm(temp1, ",");
-    result = ft_strjoinwcm(str_cat, temp);
-    ft_strdel(&content);
+	temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
+	str_cat = ft_strjoinwcm(temp1, ",");
+	result = ft_strjoinwcm(str_cat, temp);
+	ft_strdel(&content);
 	return (result);
 }
 
 char	*rebase_str_third_case(char *content)
 {
 	char *result;
-    char *str_cat;
+	char *str_cat;
 	char *temp;
-    char *temp1;
-	
+	char *temp1;
+
 	result = NULL;
-    str_cat = NULL;
+	str_cat = NULL;
 	temp = NULL;
-    temp1 = NULL;
+	temp1 = NULL;
 	temp = ft_strchr(content, 35);
-    temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
-    str_cat = ft_strjoinwcm(temp1, ",");
-    result = ft_strjoinwcm(str_cat, temp);
-    ft_strdel(&content);
+	temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
+	str_cat = ft_strjoinwcm(temp1, ",");
+	result = ft_strjoinwcm(str_cat, temp);
+	ft_strdel(&content);
 	return (result);
 }
 
 char	*rebase_str_fourth_case(char *content)
 {
 	char *result;
-    char *str_cat;
+	char *str_cat;
 	char *temp;
-    char *temp1;
-	
+	char *temp1;
+
 	result = NULL;
-    str_cat = NULL;
+	str_cat = NULL;
 	temp = NULL;
-    temp1 = NULL;
+	temp1 = NULL;
 	temp = ft_strchr(content, 59);
-    temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
-    str_cat = ft_strjoinwcm(temp1, ",");
-    result = ft_strjoinwcm(str_cat, temp);
-    ft_strdel(&content);
+	temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
+	str_cat = ft_strjoinwcm(temp1, ",");
+	result = ft_strjoinwcm(str_cat, temp);
+	ft_strdel(&content);
 	return (result);
 }
 
 char	*rebase_str_fifth_case(char *content)
 {
 	char *result;
-    char *str_cat;
+	char *str_cat;
 	char *temp;
-    char *temp1;
-	
+	char *temp1;
+
 	result = NULL;
-    str_cat = NULL;
+	str_cat = NULL;
 	temp = NULL;
-    temp1 = NULL;
+	temp1 = NULL;
 	temp = ft_strchr(content, 58) + 1;
-    temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
-    str_cat = ft_strjoinwcm(temp1, ",");
-    result = ft_strjoinwcm(str_cat, temp);
-    ft_strdel(&content);
+	temp1 = ft_strsub(content, 0, strlen(content) - strlen(temp));
+	str_cat = ft_strjoinwcm(temp1, ",");
+	result = ft_strjoinwcm(str_cat, temp);
+	ft_strdel(&content);
 	return (result);
 }
--- src/validation.c
+++ src/validation.c
@@ -6,7 +6,7 @@
 /*   By: idunaver <idunaver@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 19:46:06 by idunaver          #+#    #+#             */
-/*   Updated: 2020/02/18 19:46:07 by idunaver         ###   ########.fr       */
+/*   Updated: 2020/02/20 20:01:42 by idunaver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ char	*help_check_name_or_comment_champs(t_asm_content *content, int fd)
 			return (temp);
 		}
 		temp = ft_strjoinwcm(temp, "\n");
-	} while (get_next_line(fd, &content->line) > 0);
+	}
+	while (get_next_line(fd, &content->line) > 0);
 	return (NULL);
 }
 
@@ -56,15 +57,16 @@ char	*help_validation(char *content)
 
 char	*check_valid(t_asm_content *content, int fd)
 {
-	if (parse(content->line, PATTERN_SPACE_OR_EMPTY_LINE)){
+	if (parse(content->line, PATTERN_SPACE_OR_EMPTY_LINE))
+	{
 		return (content->line);
 	}
 	else if (parse(content->line, PATTERN_NAME_OR_COMMENT_CHAMPS_FIRST_STAGE))
 		return (help_check_name_or_comment_champs(content, fd));
 	else if (parse(content->line, PATTERN_COMMENT))
 		return (content->line);
-	else if (content->flag_name && content->flag_comment && parse\
-	(content->line, PATTERN))
+	else if (content->flag_name && content->flag_comment &&
+	parse(content->line, PATTERN))
 	{
 		content->flag_pattern = 1;
 		content->line = help_validation(content->line);
@@ -78,7 +80,6 @@ char	*search_label(t_strings *rows, char *content)
 {
 	t_token		*pointer;
 	t_strings	*struct_pointer;
-	
 
 	struct_pointer = rows;
 	while (struct_pointer)
@@ -108,9 +109,9 @@ char	*check_all_label(t_strings *rows, t_asm_content **struct_content)
 		pointer = struct_pointer->string;
 		while (pointer)
 		{
-			if ((!ft_strcmp(pointer->type, DIRECT_LABEL_NAME) || !ft_strcmp\
-			(pointer->type, INDIRECT_LABEL_NAME)) && !search_label\
-			(rows, pointer->content))
+			if ((!ft_strcmp(pointer->type, DIRECT_LABEL_NAME) ||
+			!ft_strcmp(pointer->type, INDIRECT_LABEL_NAME)) &&
+			!search_label(rows, pointer->content))
 				return (NULL);
 			flag = check_all_label_size(pointer, struct_content, flag);
 			pointer = pointer->next;
