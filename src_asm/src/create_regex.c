f123c6eaa56705e865a725cbb99d6fafdffa6d97
Iron emmett Dunaver <idunaver@il-k2.21-school.ru>
2020-03-11 21:23:53 +0300

    norminette finish

--- src_asm/src/create_regex.c
+++ src_asm/src/create_regex.c
@@ -6,7 +6,7 @@
 /*   By: idunaver <idunaver@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 19:28:33 by idunaver          #+#    #+#             */
-/*   Updated: 2020/03/11 20:43:21 by idunaver         ###   ########.fr       */
+/*   Updated: 2020/03/11 21:12:20 by idunaver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,18 @@ void	create_regex(t_asm_content *content)
 	content->regex_array[INDIRECT_LABEL] = create_indir_label_result(content);
 	content->regex_array[INDIRECT] = create_indir_result(content);
 	content->regex_array[PATTERN_NAME_CHAMPS] = create_name_champ(content);
-	content->regex_array[PATTERN_COMMENT_CHAMPS] = create_comment_champ(content);
+	content->regex_array[PATTERN_COMMENT_CHAMPS] =
+	create_comment_champ(content);
 	content->regex_array[PATTERN_COMMENT] = create_comment(content);
-	content->regex_array[PATTERN_SPACE_OR_EMPTY_LINE] = create_pattern_space_or_empty_line(content);
+	content->regex_array[PATTERN_SPACE_OR_EMPTY_LINE] =
+	create_pattern_space_or_empty_line(content);
 	content->regex_array[PATTERN] = create_pattern(content);
-	content->regex_array[PATTERN_NAME_OR_COMMENT_CHAMPS] = create_pattern_name_or_comment_champs(content);
-	content->regex_array[PATTERN_HELP_VALIDATION_FIRST_CASE] = create_pattern_first(content);
-	content->regex_array[PATTERN_HELP_VALIDATION_SECOND_CASE] = create_pattern_second(content);
-	content->regex_array[PATTERN_HELP_VALIDATION_THIRD_CASE] = create_pattern_third(content);
+	content->regex_array[PATTERN_NAME_OR_COMMENT_CHAMPS] =
+	create_pattern_name_or_comment_champs(content);
+	content->regex_array[PATTERN_HELP_VALIDATION_FIRST_CASE] =
+	create_pattern_first(content);
+	content->regex_array[PATTERN_HELP_VALIDATION_SECOND_CASE] =
+	create_pattern_second(content);
+	content->regex_array[PATTERN_HELP_VALIDATION_THIRD_CASE] =
+	create_pattern_third(content);
 }
--- src_asm/src/label_validation.c
+++ src_asm/src/label_validation.c
@@ -6,13 +6,19 @@
 /*   By: idunaver <idunaver@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 19:46:06 by idunaver          #+#    #+#             */
-/*   Updated: 2020/03/11 20:23:18 by idunaver         ###   ########.fr       */
+/*   Updated: 2020/03/11 21:22:04 by idunaver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-char	*label_validation(t_token *pointer, char *content,
+static	void	clean_help_norminette(char *temp, char *temp1)
+{
+	ft_strdel(&temp);
+	ft_strdel(&temp1);
+}
+
+char			*label_validation(t_token *pointer, char *content,
 							t_asm_content *struct_content)
 {
 	char		*temp;
@@ -20,30 +26,29 @@ char	*label_validation(t_token *pointer, char *content,
 
 	temp = NULL;
 	temp1 = NULL;
-	if (!ft_strcmp(pointer->type, LABEL_NAME) && parse(content, struct_content->regex_array[DIRECT_LABEL]) \
-	&& !ft_strcmp((temp = ft_strsub(pointer->content, 0, ft_strlen(\
-	pointer->content) - 1)), (temp1 = ft_strsub(content, 2, ft_strlen(content)))))
+	if (!ft_strcmp(pointer->type, LABEL_NAME) &&
+	parse(content, struct_content->regex_array[DIRECT_LABEL]) && !ft_strcmp(
+	(temp = ft_strsub(pointer->content, 0, ft_strlen(pointer->content) - 1)),
+	(temp1 = ft_strsub(content, 2, ft_strlen(content)))))
 	{
-		ft_strdel(&temp);
-		ft_strdel(&temp1);
+		clean_help_norminette(temp, temp1);
 		return (content);
 	}
-	else if (!ft_strcmp(pointer->type, LABEL_NAME) && parse(content,\
-	struct_content->regex_array[INDIRECT_LABEL]) && !ft_strcmp((temp = ft_strsub(pointer->content, 0,\
-	ft_strlen(pointer->content) - 1)), (temp1 = ft_strsub(content, 1, \
+	else if (!ft_strcmp(pointer->type, LABEL_NAME) && parse(content,
+	struct_content->regex_array[INDIRECT_LABEL]) &&
+	!ft_strcmp((temp = ft_strsub(pointer->content, 0,
+	ft_strlen(pointer->content) - 1)), (temp1 = ft_strsub(content, 1,
 	ft_strlen(content)))))
 	{
-		ft_strdel(&temp);
-		ft_strdel(&temp1);
+		clean_help_norminette(temp, temp1);
 		return (content);
 	}
-	ft_strdel(&temp);
-	ft_strdel(&temp1);
+	clean_help_norminette(temp, temp1);
 	return (NULL);
 }
 
-int		check_all_label_size(t_token *pointer, t_asm_content **struct_content,\
-int flag)
+int				check_all_label_size(t_token *pointer,
+t_asm_content **struct_content, int flag)
 {
 	if (!ft_strcmp(pointer->type, INSTRACTION_NAME))
 	{
@@ -55,10 +60,10 @@ int flag)
 	}
 	else if (!ft_strcmp(pointer->type, REGISTER_NAME))
 		(*struct_content)->memory_code_size += ONE_BYTE;
-	else if (!ft_strcmp(pointer->type, INDIRECT_NAME) || \
+	else if (!ft_strcmp(pointer->type, INDIRECT_NAME) ||
 	!ft_strcmp(pointer->type, INDIRECT_LABEL_NAME))
 		(*struct_content)->memory_code_size += TWO_BYTE;
-	else if (!ft_strcmp(pointer->type, DIRECT_NAME) || \
+	else if (!ft_strcmp(pointer->type, DIRECT_NAME) ||
 	!ft_strcmp(pointer->type, DIRECT_LABEL_NAME))
 		(*struct_content)->memory_code_size += flag ? FOUR_BYTE : TWO_BYTE;
 	pointer->memory_size = (*struct_content)->memory_code_size;
